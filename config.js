10778b40c2c688d536fb6681228da8205d2dfcfc
BlackShark96 <85300501+BlackShark96@users.noreply.github.com>
2021-06-03 21:47:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Em yêu à!',
+    introTitle: 'Em yêu của anh à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
